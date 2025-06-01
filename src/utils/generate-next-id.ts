ac56e637645520cb9758ae2c8f8aaba17d026420
ionut <ionut_ilisei@yahoo.com>
2025-06-01 19:15:21 +0300

    chore(env): update API_URL

--- src/utils/generate-next-id.ts
+++
@@ -1,4 +0,0 @@
-export function generateNextId<T>(items: T[], key: (item: T) => string): string {
-  const maxId = Math.max(...items.map((item) => parseInt(String(key(item)))))
-  return (maxId + 1).toString()
-}
